bf5850bfffb05001611389ca99361b4459af332b
Muhammad Mabrouk <muhammad.mabrouk.web.designer@gmail.com>
2023-09-28 18:03:27 +0300

    feat(fe): add font family based on language

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalizationService, DomInsertionService, CONTENT_STRATEGY } from '@abp/ng.core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
     <abp-dynamic-layout></abp-dynamic-layout>
   `,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor(
+    private localizationSer: LocalizationService,
+    private domInsertionSer: DomInsertionService,
+  ) {}
+
+  ngOnInit() {
+    // initialize app fonts
+    this.initAppFonts();
+  }
+
+  // initialize app fonts
+  initAppFonts() {
+    let fontsStyle: HTMLStyleElement;
+
+    const setFontsStyle = () => {
+      fontsStyle = this.domInsertionSer.insertContent(
+        CONTENT_STRATEGY.AppendStyleToHead(`
+          @import url(${this.localizationSer.instant('::Font:Url')});
+          body { font-family: ${this.localizationSer.instant('::Font:Family')}; }
+        `)
+      );
+    };
+
+    // set fonts style initially
+    setFontsStyle();
+
+    // set fonts style on language change
+    this.localizationSer.languageChange$.subscribe(() => {
+      fontsStyle && this.domInsertionSer.removeContent(fontsStyle);
+
+      setFontsStyle();
+    });
+  }
+}
