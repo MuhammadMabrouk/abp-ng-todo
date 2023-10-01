02e891b629fa84a809d91d6c9b8ba30ca3eba841
Muhammad Mabrouk <muhammad.mabrouk.web.designer@gmail.com>
2023-10-01 18:26:34 +0300

    feat(fe): add identity-extended module 2

--- angular/src/app/modules/identity-extended/contributors/entity-action-contributors.ts
+++ angular/src/app/modules/identity-extended/contributors/entity-action-contributors.ts
@@ -5,9 +5,13 @@ import { IdentityExtendedComponent } from '../identity-extended.component';
 
 const quickViewAction = new EntityAction<IdentityUserDto>({
   text: 'Identity::QuickView',
-  action: data => {
+  icon: 'fas fa-bolt',
+  action: (data) => {
     const component = data.getInjected(IdentityExtendedComponent);
     component.openUserQuickView(data.record);
+  },
+  visible: (data) => {
+    return data.record.isActive;
   },
 });
 
